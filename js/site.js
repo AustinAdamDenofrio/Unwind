f20215e5f46790039e636c1f2f29a8bcca5609db
Austin Denofrio DT <denofrioaustin@gmail.com>
2024-06-23 17:03:56 -0400

    Added code section, repo link, custom logos

--- js/site.js
+++ js/site.js
@@ -1,29 +1,16 @@
 function getValues() {
-
-    // input = 'OLYMPUS'
     let inputString = document.getElementById('inputString').value;
     let stringResult = reverseString(inputString);
     displayString(stringResult);
 }
 
-function reverseString(inputString){
-    
+function reverseString(inputString){    
     let result= '';
-
-    for (let i = inputString.length-1;i >= 0;i--) {
-        
-        result += inputString[i];
-        
-    }
-
+    for (let i = inputString.length-1;i >= 0;i--) result += inputString[i];
     return result;
 }
 
-function displayString(stringResult) {
-    
+function displayString(stringResult) {    
     document.getElementById('alert').classList.remove('invisible');
-    document.getElementById('msg').innerHTML =`your string as been reversed to <b>${stringResult}</b>`;
-
-
- 
+    document.getElementById('msg').innerHTML =`your string as been reversed to <b>${stringResult}</b>`; 
 }
